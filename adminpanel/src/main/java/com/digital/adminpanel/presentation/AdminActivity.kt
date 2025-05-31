27dc832dd89783859db980ee37eb036072baddf8
Winnkkkk <tivchenko5442@gmail.com>
2025-05-31 17:57:35 +0300

    micro fixes

--- adminpanel/src/main/java/com/digital/adminpanel/presentation/AdminActivity.kt
+++ adminpanel/src/main/java/com/digital/adminpanel/presentation/AdminActivity.kt
@@ -3,7 +3,6 @@ package com.digital.adminpanel.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,7 +15,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class AdminActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MeditationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
